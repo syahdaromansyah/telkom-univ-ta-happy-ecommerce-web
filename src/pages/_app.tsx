ce747da73be684904d78378237e6823b08939bca
Syahda Romansyah <syahdarom@gmail.com>
2023-03-16 23:03:18 +0700

    feat: wrap all pages into LayoutPage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import LayoutPage from '@/components/LayoutPage';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function CustomApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <LayoutPage>
+      <Component {...pageProps} />
+    </LayoutPage>
+  );
 }

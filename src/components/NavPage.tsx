631fa42c18e2d1bffcd464236c01fc18761d0cb7
Syahda Romansyah <syahdarom@gmail.com>
2023-05-07 22:08:20 +0700

    feat: update NavPage logic functionalities

--- src/components/NavPage.tsx
+++ src/components/NavPage.tsx
@@ -1,15 +1,46 @@
 import happyLightLogo from '@/assets/logos/happy-logo-light.png';
+import config from '@/config/config';
+import {
+  useAppDispatch,
+  useAppSelector,
+} from '@/redux-app/redux-typed-hook/typedHooks';
+import {
+  authSelector,
+  setEmail,
+  setFullname,
+  setId,
+  setUsername,
+} from '@/redux-app/slices/authSlice';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
 import cn from 'classnames';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 export default function NavPage() {
   const [openNavBoard, setOpenNavBoard] = useState<boolean>(() => false);
 
+  const nextRouter = useRouter();
+
+  const { id, fullname } = useAppSelector(authSelector);
+  const reduxDispatch = useAppDispatch();
+
   const navBoardHandler = () => setOpenNavBoard((prevValue) => !prevValue);
+  const logoutHandler = async () => {
+    await axios.get(`${config.HAPPY_BASE_URL_API}/logout`, {
+      withCredentials: true,
+    });
+
+    reduxDispatch(setId(''));
+    reduxDispatch(setFullname(''));
+    reduxDispatch(setUsername(''));
+    reduxDispatch(setEmail(''));
+
+    await nextRouter.replace('/login');
+  };
 
   return (
     <nav>
@@ -50,30 +81,64 @@ export default function NavPage() {
             )}
           >
             <div className="rounded-md border-2 border-zinc-600 bg-zinc-800 px-2 py-4 md:border-4">
-              <div className="mb-4 hidden rounded-md bg-zinc-600 px-3 py-2">
-                <p className="font-poppins text-lg font-semibold">Nama Akun</p>
-                <p></p>
-              </div>
+              {id !== '' && (
+                <>
+                  <div className="mb-4 rounded-md px-3 py-2">
+                    <p className="text-center font-poppins text-lg font-semibold">
+                      Halo! {fullname}
+                    </p>
+                    <p></p>
+                  </div>
+                </>
+              )}
 
               <div>
                 <ul className="grid gap-y-3">
-                  <li>
-                    <Link
-                      className="inline-block w-full rounded-md bg-zinc-700 py-2 text-center font-poppins font-semibold text-zinc-100 md:text-lg"
-                      href="/login"
-                    >
-                      Masuk
-                    </Link>
-                  </li>
+                  {id === '' && (
+                    <>
+                      <li>
+                        <Link
+                          className="inline-block w-full rounded-md bg-zinc-700 py-2 text-center font-poppins font-semibold text-zinc-100 md:text-lg"
+                          href="/login"
+                        >
+                          Masuk
+                        </Link>
+                      </li>
 
-                  <li>
-                    <Link
-                      className="inline-block w-full rounded-md bg-zinc-700 py-2 text-center font-poppins font-semibold text-zinc-100 md:text-lg"
-                      href="/register"
-                    >
-                      Daftar Akun
-                    </Link>
-                  </li>
+                      <li>
+                        <Link
+                          className="inline-block w-full rounded-md bg-zinc-700 py-2 text-center font-poppins font-semibold text-zinc-100 md:text-lg"
+                          href="/register"
+                        >
+                          Daftar Akun
+                        </Link>
+                      </li>
+                    </>
+                  )}
+
+                  {id !== '' && (
+                    <>
+                      <li>
+                        <Link
+                          className="inline-block w-full rounded-md bg-zinc-700 py-2 text-center font-poppins font-semibold text-zinc-100 md:text-lg"
+                          href="/transactions"
+                        >
+                          Invoices
+                        </Link>
+                      </li>
+
+                      <li>
+                        <button
+                          type="button"
+                          className="inline-block w-full rounded-md bg-rose-700 py-2 text-center font-poppins font-semibold text-zinc-100 md:text-lg"
+                          disabled={id === ''}
+                          onClick={logoutHandler}
+                        >
+                          Keluar
+                        </button>
+                      </li>
+                    </>
+                  )}
                 </ul>
               </div>
             </div>

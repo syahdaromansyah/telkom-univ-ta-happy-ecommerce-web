d9600ff03f1f5119bbc2208f92f35ff6377f9def
Syahda Romansyah <syahdarom@gmail.com>
2023-05-13 23:50:12 +0700

    chore: create POST /register endpoint on msw handler

--- src/mocks/msw/handlers.ts
+++ src/mocks/msw/handlers.ts
@@ -5,6 +5,13 @@ interface LoginRequestBody {
   password: string;
 }
 
+interface RegisterRequestBody {
+  username: string;
+  fullname: string;
+  email: string;
+  password: string;
+}
+
 export const handlers = [
   rest.post('/login', async (req, res, ctx) => {
     const { username, password } = await req.json<LoginRequestBody>();
@@ -35,6 +42,43 @@ export const handlers = [
             username === 'foobar'
               ? 'password is unmatched'
               : 'username account is not registered',
+        })
+      );
+    }
+  }),
+  rest.post('/register', async (req, res, ctx) => {
+    const { username, email } = await req.json<RegisterRequestBody>();
+    const registerIsSuccess =
+      username !== 'foobar' && email !== 'foobar@acme.com';
+
+    if (registerIsSuccess) {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          code: 200,
+          status: 'success',
+        })
+      );
+    }
+
+    if (username === 'foobar') {
+      return res(
+        ctx.status(409),
+        ctx.json({
+          code: 409,
+          status: 'failed',
+          data: 'username has been used',
+        })
+      );
+    }
+
+    if (email === 'foobar@acme.com') {
+      return res(
+        ctx.status(409),
+        ctx.json({
+          code: 409,
+          status: 'failed',
+          data: 'email has been used',
         })
       );
     }

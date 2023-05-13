c02771517c8bb1dbb1f4a487cecda9cca0494c8f
Syahda Romansyah <syahdarom@gmail.com>
2023-05-13 13:32:08 +0700

    chore: update store prop value for Redux Provider on _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import LayoutPage from '@/components/LayoutPage';
-import { reduxStore } from '@/redux-app/redux-store/store';
+import { setupStore } from '@/redux-app/redux-store/store';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Provider as ReduxProvider } from 'react-redux';
 
 export default function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <ReduxProvider store={reduxStore}>
+    <ReduxProvider store={setupStore()}>
       <LayoutPage>
         <Component {...pageProps} />
       </LayoutPage>
